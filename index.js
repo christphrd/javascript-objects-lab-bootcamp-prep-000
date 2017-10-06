b54d307c29f3f6cabfba72e497f32033fc9e90ef
Christopher Diep <diep.christopher@gmail.com>
2017-10-06 23:54:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  object = delete object.key
+  delete object.key
   return object
 }
 
